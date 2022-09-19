d94b7905ea9ffffac913ad8c50315f34335f7293
Sittichai <sittichai.sa@rmutsvmail.com>
2022-09-19 16:20:33 +0700

    Create hotel.php

---
+++ hotel.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?php include "includes/header.php"; ?>
+
+  <head>
+
+  <body>
+    <?php include "includes/navbar.php"; ?>
+
+    <div class="container">
+      <!-- start -->
+      <h2>ที่พัก-โรงเเรม</h2>
+      <hr>
+
+    </div>
+
+  </body>
+
+</html>
+<?php include "includes/footer.php"; ?>
